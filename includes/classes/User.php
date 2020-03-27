312dbb2efa308e581e7c117857d55d65c648f34f
emkeris <emkeris@gmail.com>
2020-03-27 17:31:56 +0200

    update paded added and configured

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -15,8 +15,13 @@
         public function getFirstAndLastname() {
             $query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE userName = '$this->username'") or die(mysqli_error($this->con));
             $row = mysqli_fetch_assoc($query);
-
             return $row['name'];
+        }
+
+        public function getEmail() {
+            $query = mysqli_query($this->con, "SELECT email FROM users WHERE userName = '$this->username'") or die(mysqli_error($this->con));
+            $row = mysqli_fetch_assoc($query); 
+            return $row['email'];
         }
 
     }
--- updateDetails.php
+++ updateDetails.php
@@ -1,1 +1,22 @@
-<h1>hello</h1>
+<?php 
+    @include 'includes/includedFiles.php';
+
+?>
+
+<div class="userDetails">
+    <div class="container borderBottom">
+        <h2>Email</h2>
+        <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail()?>"> 
+        <span class="message"></span>
+        <button class="button" onclick="updateEmail('email')">Save</button>
+    </div>
+
+    <div class="container">
+        <h2>Password</h2>
+        <input type="password" class="oldPassword" name="oldPassword" placeholder="Your old Password..."> 
+        <input type="password" class="newPassword1" name="newPassword1" placeholder="Your New Password..."> 
+        <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm new password"> 
+        <span class="message"></span>
+        <button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">Save</button>
+    </div>
+</div>
