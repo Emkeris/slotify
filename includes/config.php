baf66b6943cabc732202353d24c220e222630487
emkeris <emkeris@gmail.com>
2020-03-28 14:40:16 +0200

    remoteMysqlDb

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    $con = new mysqli("localhost", "root", "", "slotify") or die(mysqli_error($con));
+    $con = new mysqli("remotemysql.com", "kI8aY7uiPB", "ybeizwC7mN", "kI8aY7uiPB") or die(mysqli_error($con));
 
     if(isset($_SESSION['userLoggedIn'])) {
         $userLoggedIn = $_SESSION['userLoggedIn'];
